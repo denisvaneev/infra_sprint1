512015ac2345115b3f7484f8e616d3cfc6db1927
denisvaneev <vaneev.denis98@yandex.ru>
2023-11-13 14:00:47 +0000

    corrections in kittigram

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = ['158.160.18.243', '127.0.0.1', 'localhost', 'kittygramyandex.hopto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
